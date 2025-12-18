62bfd4cc7c6a4037cf61839395cd9b920c8d9505
aayushma12 <aayushmaacharya@123>
2025-12-18 23:12:40 +0545

    Sprint-2: updated dashboard.dart (2/10)

---
+++ lib/screen/dashboard.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'tutor_screen.dart';
+import 'add_screen.dart';
+import 'wishlist_screen.dart';
+import 'profile_screen.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomeScreen(),
+    TutorScreen(),
+    AddScreen(),
+    WishlistScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() => _currentIndex = index);
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Tutor',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle, size: 30),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: 'Wishlist',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
